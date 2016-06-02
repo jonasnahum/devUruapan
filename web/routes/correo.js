3d77e18918924e7e64305acc6679cb107224db5c
jonasnahum <jonasnahum@gmail.com>
2016-06-02 09:29:45 -0500

    intento sin string

--- web/routes/correo.js
+++ web/routes/correo.js
@@ -16,12 +16,18 @@ router.post('/', function(req, res, next) {
                 user: 'postmaster@sandboxf9a06959cc684146bef93b1b8250ac9b.mailgun.org',
                 pass:'52e52c9ac4f8dcc9468b236345d0559b'
 
-
+                ultimo que funciono de rodrigo
                 user: "postmaster@sandboxb7d3126ecba24dcabf57d1a8dcec11cb.mailgun.org",
                 pass: "30c5e730f5a5ef584ffcc8a0dccd079f"
-                */
+
+                ultimo que funciono combinado
                 user: process.env.EMAIL_USER || 'postmaster@sandboxf9a06959cc684146bef93b1b8250ac9b.mailgun.org',
                 pass: process.env.EMAIL_PASS || '52e52c9ac4f8dcc9468b236345d0559b'
+                */
+
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS 
+
             }
         });
     console.log(nodemailer);
